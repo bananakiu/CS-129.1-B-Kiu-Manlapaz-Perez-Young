82e432be8c4a81b9105248e3443ced5e019e2276
Leandre <leandrenoelkiu@gmail.com>
2017-12-08 12:55:36 +0800

    Add files via upload

---
+++ cfg.js
@@ -0,0 +1,21 @@
+var config = {
+  "_id": "replicate",
+  "version" : 1,
+  "members" :   [
+    {
+      "_id" : 0,
+      "host" : "localhost:27017",
+      "priority" : 1
+    },
+    {
+      "_id" : 1,
+      "host" : "localhost:27018",
+      "priority" : 0
+    },
+    {
+      "_id" : 2,
+      "host" : "localhost:27019",
+      "priority" : 0
+    }
+  ]
+}
